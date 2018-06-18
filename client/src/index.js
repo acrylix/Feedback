cc5e240f4254a0b9a711228d25375eafc99d94ce
Acrylix <1619025+acrylix@users.noreply.github.com>
2018-06-18 14:14:49 -0400

    basic reducers set up

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
 
-ReactDOM.render(<App/>,document.querySelector('#root'));
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+  //provider comonents detects new states in store and alert all child components of changes
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
