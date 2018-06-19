85c2df1649e5280681f4890657bcea2a3fe906b4
Acrylix <1619025+acrylix@users.noreply.github.com>
2018-06-19 02:25:33 -0400

    temp show credit in header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -16,6 +16,9 @@ class Header extends Component {
         );
       default:
         return [
+          <li key="3">
+            {this.props.auth.credits}
+          </li>,
           <li key="1">
             <Payments />
           </li>,
