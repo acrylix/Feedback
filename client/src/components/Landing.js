0554c00ed43b56504d5aaf8ed8b601e2590e8107
acrylix <1619025+acrylix@users.noreply.github.com>
2018-06-24 17:03:37 -0400

    landing page add on

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
     <div style={{ textAlign: 'center' }}>
       <h1>Feedback</h1>
-      Collect feedback from your users
+      <p>Collect feedback from your users</p>
+      <Link to="/surveys" className="btn white-text">
+        Dashbaord
+      </Link>
     </div>
   );
 };
